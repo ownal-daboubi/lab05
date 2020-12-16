b204e0b9b565606135d868487ca0b258fd8912cf
ownal-daboubi <owndaboubii@gmil.com>
2020-12-16 12:55:54 +0200

    adding files

---
+++ myscript.js
@@ -0,0 +1,20 @@
+var name = prompt("please enter your name");
+
+alert( "hello"  + name);
+
+var age = prompt("please enter your age");
+
+if(age>18){
+
+    alert("you are allowed to enter the website");
+}
+else{
+alert("you are not allowed to enter the webite");
+}
+
+
+
+
+
+
+
